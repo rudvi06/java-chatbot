8c43a7f1eed04f6287f2cb0a7bf1d2c0a121ba3f
Rudvi Manjrekar <rudvi306@gmail.com>
2022-10-12 17:16:29 +0530

    Initial b commit

--- src/main/java/com/ecommerce/mvc/controllers/HomeController.java
+++ src/main/java/com/ecommerce/mvc/controllers/HomeController.java
@@ -1,4 +1,11 @@
 package com.ecommerce.mvc.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
 }
