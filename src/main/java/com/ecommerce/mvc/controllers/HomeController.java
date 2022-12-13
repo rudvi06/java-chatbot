c3650dd8c37d8fdda7e7917b6e6999de2b810532
Rudvi Manjrekar <rudvi306@gmail.com>
2022-12-13 17:00:15 +0530

    removed chatbot

--- src/main/java/com/ecommerce/mvc/controllers/HomeController.java
+++ src/main/java/com/ecommerce/mvc/controllers/HomeController.java
@@ -1,28 +1,14 @@
 package com.ecommerce.mvc.controllers;
 
-import com.ecommerce.mvc.services.ChatBotService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @Autowired
-    private ChatBotService chatBotService;
-
     @GetMapping("/")
     public String home(){
 
-        return "home";
-    }
-
-    @PostMapping("/")
-    @ResponseBody
-    public String home(String message){
-        chatBotService.chatBot(message);
         return "home";
     }
 
